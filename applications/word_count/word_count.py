76782c507215ceccaed52f3b7f0423a31749a5f1
Kelli Scheuble <github@scheuble.dev>
2020-08-05 15:34:23 -0700

    word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,21 @@
+import re
+
 def word_count(s):
-    # Your code here
+    words = s.split()
+    word_dict = dict()
 
+    for word in words:
+        word = word.lower()
+        replace = ['"',':', ';', ',', '.', '-', '+', '=', '/', "\"", '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+        for character in replace:
+            word = word.replace(character, "")
 
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+
+    return word_dict
 
 if __name__ == "__main__":
     print(word_count(""))
