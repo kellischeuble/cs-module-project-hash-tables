c1bd21665237cf2c4e357dc99b00d8132f0adc4c
Kelli Scheuble <github@scheuble.dev>
2020-08-06 15:21:19 -0700

    Solution for histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,25 @@
-# Your code here
+with open("robin.txt") as f:
+    words = f.read()
+
+words = words.split()
+
+word_dict = dict()
+
+for word in words:
+    word = word.lower()
+
+    characters = """:;,."-+=/\|[]{}()*^&"""
+    for c in characters:
+       word = word.replace(c, '')
+
+    if word not in word_dict:
+        word_dict[word] = 0
+    word_dict[word] += 1
+
+words = list(word_dict.items())
+words.sort(key=lambda x: (x[1], x[0]))
+
+for word in words:
+    print(word[0] + " " * (15 - len(word[0])) + "#" * word[1])
+    
 
